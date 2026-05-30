1be2cf61a3ed566248dc46d95816014b83670378
lance-pyles <36748284+lance-pyles@users.noreply.github.com>
2026-05-29 21:54:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.get("/test", async (req, res) => { return "success"; });
+app.get("/test", async (req, res) => { "success"; });
 
 app.listen(3000, () => { console.log("Screenshot API running on port 3000"); });
