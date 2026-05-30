2de68c3803e0099e5ee88edcc0926c94a65a70d1
lance-pyles <36748284+lance-pyles@users.noreply.github.com>
2026-05-29 22:42:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
 
+const express = require('express');
+
 const app = express();
+
+// Parse JSON request bodies
+app.use(express.json());
 
 app.get("/test", async (req, res) => {
 
-  const name = req.query.name;
-  const lname = req.query.lname;
+  const name = req.query.name ?? req.body.name;
+  const lname = req.query.lname ?? req.body.lname;
   
   res.json({message: 'Hello, ' + name + ' ' + lname + '!'});
 
