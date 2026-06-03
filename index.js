940b07a4f1fbfd745b4be936ac46e4621c4e0b1e
lance-pyles <36748284+lance-pyles@users.noreply.github.com>
2026-06-03 09:38:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
   intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent
   ]
 });
