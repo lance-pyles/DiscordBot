54aded1ebbd7c29f54ee724f65eb54c44cf4d33e
lance-pyles <36748284+lance-pyles@users.noreply.github.com>
2026-06-07 21:16:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ function generatePassword(length, allowNumbers, allowLetters) {
         password: password,
         combinations: combinations,
         charset: charset,
+        possibleCharacters: charset.length
         allowLetters: allowLetters,
         allowNumbers: allowNumbers,
         length: length
