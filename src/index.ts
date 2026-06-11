1dddbace773c52b40b4d8b7092b451965a2fa836
lance-pyles <36748284+lance-pyles@users.noreply.github.com>
2026-06-10 19:22:47 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,7 @@ function generatePassword(length?: number, allowNumbers?: boolean, allowLetters?
   if (length === null) { if (error === null) {error = "Length must be provided.";} else {error += "Length must be provided.";} }
   if (length === 0) { if (error === null) {error = "Length must be greater than 0.";} else {error += "Length must be greater than 0.";} }
   if (charset === null) { if (error === null) {error = "Character set must be provided.";} else {error += "Character set must be provided.";} }
-  if (charset.length === 0) { if (error === null) {error = "Characters set length must be greater than 0.";} else {error += "Characters set length must be greater than 0."";} }
+  if (charset.length === 0) { if (error === null) {error = "Characters set length must be greater than 0.";} else {error += "Characters set length must be greater than 0.";} }
 
   if error != null { return { success: false,  error: error }; }
   
